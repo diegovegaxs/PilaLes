979e090d474272cbb3c35c4511af8e825e03a7e7
diegovegaxs <diegovegaxs@gmail.com>
2025-04-25 23:05:49 -0500

    Agregar archivos de proyecto.

---
+++ PilaLes/PilaLES.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilales
+{
+    class PilaLES
+    {
+        private string[] elementos = new string[0];
+
+        public void Push(string valor)
+        {
+            Array.Resize(ref elementos, elementos.Length + 1);
+            elementos[elementos.Length - 1] = valor;
+        }
+        public string Pop()
+        {
+            if (elementos.Length == 0)
+            {
+                Console.WriteLine("La pila está vacía");
+                return null;
+            }
+            else
+            {
+                string valor = elementos[elementos.Length - 1];
+                Array.Resize(ref elementos, elementos.Length - 1);
+                return valor;
+            }
+        }
+        public string Top()
+        {
+            if (elementos.Length == 0)
+            {
+                Console.WriteLine("La pila está vacía");
+                return null;
+            }
+            else
+            {
+                return elementos[elementos.Length - 1];
+            }
+        }
+        public int Size()
+        {
+            {
+                return elementos.Length;
+            }
+        }
+        public bool Empty()
+        {
+            return elementos.Length == 0;
+        }
+
+        public override string? ToString()
+        {
+            string resultado = "";
+
+            for (int i = 0; i < elementos.Length; i++)
+            {
+                resultado += elementos[i] + ", ";
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ PilaLes/Program.cs
@@ -0,0 +1,19 @@
+﻿using pilales;
+
+PilaLES p = new PilaLES();
+
+p.Push("Hola");
+Console.WriteLine(p.ToString());
+p.Push("Mundo");
+Console.WriteLine(p.ToString());
+p.Push("C#");
+Console.WriteLine(p.ToString());
+Console.WriteLine("Se eliminó: " + p.Pop());
+Console.WriteLine(p.ToString());
+Console.WriteLine("El último elemento es: " + p.Top());
+Console.WriteLine("La cantidad de elementos son: " + p.Size());
+p.Push("Cómo");
+Console.WriteLine(p.ToString());
+p.Push("Están");
+Console.WriteLine(p.ToString());
+Console.WriteLine("¿La lista está vacia?: "+ p.Empty());
